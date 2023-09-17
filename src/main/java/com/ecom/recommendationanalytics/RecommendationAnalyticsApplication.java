0d99a32291c2cdd9293f885d52d35f45b0639f9a
Sidharth Patnaik <sidh.rapid@gmail.com>
2023-09-17 13:23:15 +0530

    initial commit

---
+++ src/main/java/com/ecom/recommendationanalytics/RecommendationAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.recommendationanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/recommendationanalytics/RecommendationAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.recommendationanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
